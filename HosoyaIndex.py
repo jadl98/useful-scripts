fb2966f0ab1dd80dfd49e5b58c8b104747a2c16a
Javier <80933419+jadl98@users.noreply.github.com>
2024-03-31 21:20:39 -0600

    Delete HosoyaIndex.py
    Just evaluate match pol at 1 instead of doing this

--- HosoyaIndex.py
+++
@@ -1,7 +0,0 @@
-# para ser usado en Sage
-
-def Hosoya(G):
-    f = sage.graphs.matchpoly.matching_polynomial(G, complement=True, name=None)
-    a = [abs(x) for x in f.coefficients()]
-    return [a, sum(a)]
-
